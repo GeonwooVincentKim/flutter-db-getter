7c9e8cff71cc68a265070ccd30e15dea14ce3a1d
GeonwooVincentKim <kdsnop@gmail.com>
2021-02-04 17:10:37 +0900

    Updating..

---
+++ lib/screen/body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String title;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
